b8b3a33824735f756697ad04b8b7f4cfe4946625
unknown <ipominova@ya.ru>
2021-11-13 20:52:03 +0700

    * step3

--- src/components/CalcStep3.tsx
+++ src/components/CalcStep3.tsx
@@ -19,15 +19,31 @@ const cults = [
 
 
 const CalcStep3 = () => {
+  const save = () => {
+    window.location.pathname = '/result';
+  }
 
   return <div className="block-3">
     <p className="form-label">Рассчет затрат на производство</p>
     <div className="types">
-      <p>Для выращивания выбранной культуры в этом районе на 1 га за сезон понадобится: <br />
-      
+      <p>Для выращивания выбранной культуры в этом районе на 1&nbsp;га за сезон понадобится: <br />
+        <ul>
+          <li><b>электроэнергии</b> - 15000 кВт</li>
+          <li><b>воды</b> - 20000 м<sup>3</sup></li>
+          <li><b>затраты на содержание оборудование</b> - 200 000 рублей</li>
+        </ul>
       </p>
       <br />
       <p>Введите цены, чтобы просчитать бизнес-план: </p>
+      <Form.Item name="priceEnergy" label="Цена на электроэнергию"  tooltip="Средняя цена в вашем районе">
+        <Input placeholder="0" />
+      </Form.Item>
+      <Form.Item name="priceWater" label="Цена на воду"  tooltip="Средняя цена в вашем районе">
+        <Input placeholder="0" />
+      </Form.Item>
+      <Form.Item>
+                <Button type="primary" className="submitButton" onClick={() => save()}>Показать бизнес-план</Button>
+              </Form.Item>
     </div>
   </div>
 }
@@ -36,7 +52,5 @@ export default CalcStep3;
 
 
 /*
-<Form.Item label="Цена на электроэнергию" required tooltip="This is a required field">
-                <Input placeholder="input placeholder" />
-              </Form.Item>  
+
 */
